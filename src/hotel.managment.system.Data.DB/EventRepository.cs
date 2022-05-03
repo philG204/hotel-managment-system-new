1409cc1725c76bfb6400bfdd93b8cc709b3949c7
Philipp Gierenz <Philipp.Gierenz@buhmann.schule>
2022-05-03 20:04:54 +0200

    Added implementation classes for Repositories of Event and Breakfast Model

---
+++ src/hotel.managment.system.Data.DB/EventRepository.cs
@@ -0,0 +1,38 @@
+﻿using hotel_managment_system;
+using hotel_managment_system_v2.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hotel.managment.system.Data.DB
+{
+    public class EventRepository : IEventRepository
+    {
+        public bool Delete(Event obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Event Get(int TId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Event> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save(Event obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
