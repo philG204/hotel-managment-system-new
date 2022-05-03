a490f0ea00280ca6bb080cd2ea274996024251a0
Philipp Gierenz <Philipp.Gierenz@buhmann.schule>
2022-05-03 20:04:05 +0200

    Added Repsoitroies and Services for Event and Breakfast Model

---
+++ src/hotel-managment-system/Repositories/IEventRepository.cs
@@ -0,0 +1,14 @@
+﻿using hotel.managment.system.Data.DB;
+using hotel_managment_system;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hotel_managment_system_v2.Repositories
+{
+    public interface IEventRepository : IRepositoryBase<int, Event>
+    {
+    }
+}
---
+++ src/hotel-managment-system/Services/IBreakfastService.cs
@@ -0,0 +1,13 @@
+﻿using hotel_managment_system_v2.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hotel_managment_system_v2.Services
+{
+    public interface IBreakfastService : IBreakfastRepository
+    {
+    }
+}
