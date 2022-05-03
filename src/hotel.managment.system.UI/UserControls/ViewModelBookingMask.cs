3ae8d04d04004c9ee666f06284949bf9d222f071
Philipp Gierenz <Philipp.Gierenz@buhmann.schule>
2022-05-03 13:51:21 +0200

    Modified BookingMask UI and ViewModel

---
+++ src/hotel.managment.system.UI/UserControls/ViewModelBookingMask.cs
@@ -0,0 +1,58 @@
+﻿using GalaSoft.MvvmLight.Command;
+using hotel.managment.system.Service;
+using hotel_managment_system;
+using hotel_managment_system.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace hotel.managment.system.UI.UserControls
+{
+    class ViewModelBookingMask
+    {
+        // Needed Services
+        private BookingService bookingService = new BookingService();
+        
+
+        private ICommand save;
+
+        private Booking model;
+
+        public ViewModelBookingMask()
+        {
+            save = new RelayCommand(SaveBooking);
+
+            //Load all neccessary objects
+        }
+
+        private void GoBack()
+        {
+            //Moves back to previous ViewModel
+        }
+        private void SaveBooking()
+        {
+            if (model.Room == null)
+            {
+                Console.WriteLine("Sie müsssen ein Zimmer buchen");
+            }
+            else
+            {
+                //Booking objeect is getting saved
+                bookingService.Save(model);
+            }
+        }
+
+        public string SelectedMethodOfPayment { get => model.MethodOfPayment; set => model.MethodOfPayment = value}
+        public Breakfast SelectedBreakfast { get => model.Breakfast; set => model.Breakfast = value; }
+        public Event SelectedEvent { get => model.Event; set => model.Event = value; }
+        public double Amount { get => model.Amount; set => model.Amount = value; }
+        public bool Settel { get => model.settel; set => model.settel = value; }
+        public DateTime DepatureTime { get => model.Depature; set => model.Depature = value; }
+        public DateTime ArrivalTime { get => model.Arrival; set => model.Arrival = value; }
+        public Employee SelectedEmployee { get => model.SubEmployee.Employee; set => model.SubEmployee.Employee = value; }
+        public Customer SelectedCustomer { get => model.Customer; set => model.Customer = value; }
+    }
+}
