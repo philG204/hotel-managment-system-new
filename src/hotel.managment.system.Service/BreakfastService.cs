642b094c1b5af46999f50566a06442ea3605fea3
Philipp Gierenz <Philipp.Gierenz@buhmann.schule>
2022-05-03 20:05:46 +0200

    Added implementation for Services of Breakfast and Event Model

---
+++ src/hotel.managment.system.Service/BreakfastService.cs
@@ -0,0 +1,26 @@
+﻿using hotel_managment_system;
+using hotel.managment.system.Data.DB;
+using hotel_managment_system_v2.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hotel.managment.system.Service
+{
+    internal class BreakfastService : IBreakfastService
+    {
+        private readonly BreakfastRepository breakfastRepository = new BreakfastRepository();
+
+        public bool Delete(Breakfast obj) => breakfastRepository.Delete(obj);
+
+        public bool Delete(int id) => breakfastRepository.Delete(id);
+
+        public Breakfast Get(int TId) => breakfastRepository.Get(TId);  
+
+        public List<Breakfast> GetAll() => breakfastRepository.GetAll();    
+
+        public bool Save(Breakfast obj) => breakfastRepository.Save(obj);
+    }
+}
---
+++ src/hotel.managment.system.Service/EventService.cs
@@ -0,0 +1,26 @@
+﻿using hotel.managment.system.Data.DB;
+using hotel_managment_system;
+using hotel_managment_system_v2.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hotel.managment.system.Service
+{
+    public class EventService : IEventService
+    {
+        private readonly EventRepository eventRepository = new EventRepository();
+
+        public bool Delete(Event obj) => eventRepository.Delete(obj);
+
+        public bool Delete(int id) => eventRepository.Delete(id);   
+
+        public Event Get(int TId) => eventRepository.Get(TId);
+
+        public List<Event> GetAll() => eventRepository.GetAll();
+
+        public bool Save(Event obj) => eventRepository.Save(obj);
+    }
+}
