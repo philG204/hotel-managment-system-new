a16d99936543261d1f2fa23109ddae26a63c73a8
Philipp Gierenz <Philipp.Gierenz@buhmann.schule>
2022-05-05 19:01:54 +0200

    Implermented Repository of Breakfast and Event

--- src/hotel.managment.system.Data.DB/BreakfastRepository.cs
+++ src/hotel.managment.system.Data.DB/BreakfastRepository.cs
@@ -2,6 +2,7 @@
 using hotel_managment_system_v2.Repositories;
 using System;
 using System.Collections.Generic;
+using System.Data.OleDb;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,27 +13,170 @@ namespace hotel.managment.system.Data.DB
     {
         public bool Delete(Breakfast obj)
         {
-            throw new NotImplementedException();
+            try
+            {
+                OleDbConnection connection = new OleDbConnection("Provider = Microsoft.ACE.OLEDB.12.0; Data Source = DB_Abrechnung.accdb");
+                connection.Open();
+
+                OleDbCommand CheckCmd = new OleDbCommand("SELECT * FROM `Frühstück` WHERE `Frühstücksnummer` = ?", connection);
+                CheckCmd.Parameters.Add(new OleDbParameter { Value = obj.BreakfastID });
+
+                var CheckDr = CheckCmd.ExecuteReader();
+
+                if (CheckDr.HasRows != false)
+                {
+                    OleDbCommand cmd = new OleDbCommand("DELETE FROM `Frühstück` WHERE `Frühstücksnummer` = ?", connection);
+                    cmd.Parameters.Add(new OleDbParameter { Value = obj.BreakfastID });
+
+                    cmd.ExecuteNonQuery();
+
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+                return false;
+            }
         }
 
         public bool Delete(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                OleDbConnection connection = new OleDbConnection("Provider = Microsoft.ACE.OLEDB.12.0; Data Source = DB_Abrechnung.accdb");
+                connection.Open();
+
+                OleDbCommand CheckCmd = new OleDbCommand("SELECT * FROM `Frühstück` WHERE `Frühstücksnummer` = ?", connection);
+                CheckCmd.Parameters.Add(new OleDbParameter { Value = id });
+
+                var CheckDr = CheckCmd.ExecuteReader();
+
+                if (CheckDr.HasRows != false)
+                {
+                    OleDbCommand cmd = new OleDbCommand("DELETE FROM `Frühstück` WHERE `Frühstücksnummer` = ?", connection);
+                    cmd.Parameters.Add(new OleDbParameter { Value = id });
+
+                    cmd.ExecuteNonQuery();
+
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+                return false;
+            }
         }
 
         public Breakfast Get(int TId)
         {
-            throw new NotImplementedException();
+            try
+            {
+                Breakfast breakFast = new Breakfast();
+                OleDbConnection connection = new OleDbConnection("Provider = Microsoft.ACE.OLEDB.12.0; Data Source = DB_Abrechnung.accdb");
+                connection.Open();
+
+                OleDbCommand cmd = new OleDbCommand("SELECT * FROM `Frühstück` WHERE `Frühstücksnummer` = ?", connection);
+                cmd.Parameters.Add(new OleDbParameter { Value = TId });
+
+                var dr = cmd.ExecuteReader();
+
+                dr.Read();
+
+                breakFast.BreakfastID = Convert.ToInt32(dr.GetValue(0));
+                breakFast.KindOfBreakfast = Convert.ToString(dr.GetValue(1));
+                breakFast.AdditionAmount = Convert.ToInt32(dr.GetValue(2));
+
+                return breakFast;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
         public List<Breakfast> GetAll()
         {
-            throw new NotImplementedException();
+            try
+            {
+                List<Breakfast> breakFasts = new List<Breakfast>();
+                OleDbConnection connection = new OleDbConnection("Provider = Microsoft.ACE.OLEDB.12.0; Data Source = DB_Abrechnung.accdb");
+                connection.Open();
+
+                OleDbCommand cmd = new OleDbCommand("SELECT * FROM `Frühstück`", connection);
+
+                var dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    Breakfast breakFast = new Breakfast();
+                    breakFast.BreakfastID = Convert.ToInt32(dr.GetValue(0));
+                    breakFast.KindOfBreakfast = Convert.ToString(dr.GetValue(1));
+                    breakFast.AdditionAmount = Convert.ToInt32(dr.GetValue(2));
+
+                    breakFasts.Add(breakFast);
+                }
+                return breakFasts;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
         public bool Save(Breakfast obj)
         {
-            throw new NotImplementedException();
+            try
+            {
+                OleDbConnection connection = new OleDbConnection("Provider = Microsoft.ACE.OLEDB.12.0; Data Source = DB_Abrechnung.accdb");
+                connection.Open();
+
+                OleDbCommand CheckCmd = new OleDbCommand("SELECT * FROM `Frühstück` WHERE `Frühstücksnummer` = ?", connection);
+                CheckCmd.Parameters.Add(new OleDbParameter { Value = obj.BreakfastID });
+
+                var CheckDr = CheckCmd.ExecuteReader();
+
+                if (CheckDr.HasRows == false)
+                {
+
+                    OleDbCommand cmd = new OleDbCommand("INSERT INTO `Frühstück` (`Frühstücksnummer`, `Frühstücksart`, `Zusatzbetrag`) VALUES (?, ?, ?)", connection);
+                    cmd.Parameters.Add(new OleDbParameter { Value = obj.BreakfastID });
+                    cmd.Parameters.Add(new OleDbParameter { Value = obj.KindOfBreakfast });
+                    cmd.Parameters.Add(new OleDbParameter { Value = obj.AdditionAmount });
+
+                    cmd.ExecuteNonQuery();
+
+                    OleDbCommand Cmd1 = new OleDbCommand("SELECT * FROM `Aktion` ", connection);
+                    Cmd1.ExecuteReader();
+
+                    return true;
+                }
+                else
+                {
+                    OleDbCommand cmd = new OleDbCommand("UPDATE `Frühstück` SET `Frühstücksname` = ?, `Zusatzbetrag` = ? WHERE `Frühstücksnummer = ?`", connection);    
+                    cmd.Parameters.Add(new OleDbParameter { Value = obj.KindOfBreakfast });
+                    cmd.Parameters.Add(new OleDbParameter { Value = obj.AdditionAmount });
+                    cmd.Parameters.Add(new OleDbParameter { Value = obj.BreakfastID });
+
+                    cmd.ExecuteNonQuery();
+
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+                return false;
+            }
         }
     }
 }
--- src/hotel.managment.system.Data.DB/EventRepository.cs
+++ src/hotel.managment.system.Data.DB/EventRepository.cs
@@ -2,6 +2,7 @@
 using hotel_managment_system_v2.Repositories;
 using System;
 using System.Collections.Generic;
+using System.Data.OleDb;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,27 +13,174 @@ namespace hotel.managment.system.Data.DB
     {
         public bool Delete(Event obj)
         {
-            throw new NotImplementedException();
+            try
+            {
+                OleDbConnection connection = new OleDbConnection("Provider = Microsoft.ACE.OLEDB.12.0; Data Source = DB_Abrechnung.accdb");
+                connection.Open();
+
+                OleDbCommand CheckCmd = new OleDbCommand("SELECT * FROM `Mitarbeiter` WHERE `Mitarbeiternummer` = ?", connection);
+                CheckCmd.Parameters.Add(new OleDbParameter { Value = obj.EventID });
+
+                var CheckDr = CheckCmd.ExecuteReader();
+
+                if (CheckDr.HasRows != false)
+                {
+                    OleDbCommand cmd = new OleDbCommand("DELETE FROM `Mitarbeiter` WHERE `Mitarbeiternummer` = ?", connection);
+                    cmd.Parameters.Add(new OleDbParameter { Value = obj.EventID });
+
+                    cmd.ExecuteNonQuery();
+
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+                return false;
+            }
         }
 
         public bool Delete(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                OleDbConnection connection = new OleDbConnection("Provider = Microsoft.ACE.OLEDB.12.0; Data Source = DB_Abrechnung.accdb");
+                connection.Open();
+
+                OleDbCommand CheckCmd = new OleDbCommand("SELECT * FROM `Aktion` WHERE `Aktionsnummer` = ?", connection);
+                CheckCmd.Parameters.Add(new OleDbParameter { Value = id });
+
+                var CheckDr = CheckCmd.ExecuteReader();
+
+                if (CheckDr.HasRows != false)
+                {
+                    OleDbCommand cmd = new OleDbCommand("DELETE FROM `Aktion` WHERE `Aktionsnummer` = ?", connection);
+                    cmd.Parameters.Add(new OleDbParameter { Value = id });
+
+                    cmd.ExecuteNonQuery();
+
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+                return false;
+            }
         }
 
         public Event Get(int TId)
         {
-            throw new NotImplementedException();
+            try
+            {
+                Event e = new Event();
+                OleDbConnection connection = new OleDbConnection("Provider = Microsoft.ACE.OLEDB.12.0; Data Source = DB_Abrechnung.accdb");
+                connection.Open();
+
+                OleDbCommand cmd = new OleDbCommand("SELECT * FROM `Aktion` WHERE `Aktionsnummer` = ?", connection);
+                cmd.Parameters.Add(new OleDbParameter { Value = TId });
+
+                var dr = cmd.ExecuteReader();
+
+                dr.Read();
+
+                e.EventID = Convert.ToInt32(dr.GetValue(0));
+                e.EventName = Convert.ToString(dr.GetValue(1));
+                e.DiscountValue = Convert.ToInt32(dr.GetValue(2));
+
+                return e;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
         public List<Event> GetAll()
         {
-            throw new NotImplementedException();
+            try
+            {
+                List<Event> es = new List<Event>();
+                OleDbConnection connection = new OleDbConnection("Provider = Microsoft.ACE.OLEDB.12.0; Data Source = DB_Abrechnung.accdb");
+                connection.Open();
+
+                OleDbCommand cmd = new OleDbCommand("SELECT * FROM `Aktion`", connection);
+
+                var dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    Event e = new Event();
+                    e.EventID = Convert.ToInt32(dr.GetValue(0));
+                    e.EventName = Convert.ToString(dr.GetValue(1));
+                    e.DiscountValue = Convert.ToInt32(dr.GetValue(2));
+
+                    es.Add(e);
+                }
+                return es;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
         public bool Save(Event obj)
         {
-            throw new NotImplementedException();
+            try
+            {
+                OleDbConnection connection = new OleDbConnection("Provider = Microsoft.ACE.OLEDB.12.0; Data Source = DB_Abrechnung.accdb");
+                connection.Open();
+
+                OleDbCommand CheckCmd = new OleDbCommand("SELECT * FROM `Aktion` WHERE `Aktionsnummer` = ?", connection);
+                CheckCmd.Parameters.Add(new OleDbParameter { Value = obj.EventID });
+
+                var CheckDr = CheckCmd.ExecuteReader();
+
+                if (CheckDr.HasRows == false)
+                {
+
+                    OleDbCommand cmd = new OleDbCommand("INSERT INTO `Aktion` (`Aktionsnummer`, `Aktionsname`, `Rabattwert`, `Aktion_von`, `Aktion_bis`) VALUES (?, ?, ?, ?, ?)", connection);
+                    cmd.Parameters.Add(new OleDbParameter { Value = obj.EventID });
+                    cmd.Parameters.Add(new OleDbParameter { Value = obj.EventName });
+                    cmd.Parameters.Add(new OleDbParameter { Value = obj.DiscountValue });
+                    cmd.Parameters.Add(new OleDbParameter { Value = obj.From });
+                    cmd.Parameters.Add(new OleDbParameter { Value = obj.To });
+
+                    cmd.ExecuteNonQuery();
+
+                    OleDbCommand Cmd1 = new OleDbCommand("SELECT * FROM `Aktion` ", connection);
+                    Cmd1.ExecuteReader();
+
+                    return true;
+                }
+                else
+                {
+                    OleDbCommand cmd = new OleDbCommand("UPDATE `Aktion` SET `Aktionsname` = ?, `Rabattwert` = ?, `Aktion_von` = ?, `Aktion_bis` = ? WHERE `Aktionsnummer = ?`", connection);
+                    cmd.Parameters.Add(new OleDbParameter { Value = obj.EventName });
+                    cmd.Parameters.Add(new OleDbParameter { Value = obj.DiscountValue });
+                    cmd.Parameters.Add(new OleDbParameter { Value = obj.From });
+                    cmd.Parameters.Add(new OleDbParameter { Value = obj.To });
+                    cmd.Parameters.Add(new OleDbParameter { Value = obj.EventID });
+
+                    cmd.ExecuteNonQuery();
+
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+                return false;
+            }
         }
     }
 }
