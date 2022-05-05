b5fbe71d0dc4ac8699269bc7a9729e22c961ac80
Philipp Gierenz <Philipp.Gierenz@buhmann.schule>
2022-05-05 21:15:42 +0200

    Modified ViewModelBookingMask

--- src/hotel.managment.system.UI/ViewModels/ViewModelBookingMask.cs
+++ src/hotel.managment.system.UI/ViewModels/ViewModelBookingMask.cs
@@ -5,6 +5,7 @@ using hotel_managment_system.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
@@ -15,24 +16,49 @@ namespace hotel.managment.system.UI.UserControls
     {
         // Needed Services
         private BookingService bookingService = new BookingService();
-        
+        private EventService eventService = new EventService();
+        private BreakfastService breakFastService = new BreakfastService();
+        private EmployeeService employeeService = new EmployeeService();    
+        private CustomerService customerService = new CustomerService();    
 
         private ICommand save;
+        private ICommand delete;
+        private ICommand edit;
+        private ICommand goBack;
 
         private Booking model;
+        private List<Event> events;
+        private List<Breakfast> breakFasts;
+        private List<Employee> employees;
+        private List<Customer> customers;
 
         public ViewModelBookingMask()
         {
-            save = new RelayCommand(SaveBooking);
+            goBack = new RelayCommand(GoBack);
+            save = new RelayCommand(Save);
+            delete = new RelayCommand(Delete);
+            edit = new RelayCommand(Edit);  
 
             //Load all neccessary objects
+            List<Event> events = eventService.GetAll();
+            List<Breakfast> breakFasts = breakFastService.GetAll();
+            List<Employee> employees = employeeService.GetAll();
+            List<Customer> customers = customerService.GetAll();
         }
 
         private void GoBack()
         {
             //Moves back to previous ViewModel
         }
-        private void SaveBooking()
+        private void Delete() 
+        {
+            //Delete...
+        }
+        private void Edit()
+        {
+            //Edit...
+        }
+        private void Save()
         {
             if (model.Room == null)
             {
@@ -44,7 +70,10 @@ namespace hotel.managment.system.UI.UserControls
                 bookingService.Save(model);
             }
         }
-
+        public List<Event> Events { get => events; }
+        public List<Breakfast> Breakfasts { get => breakFasts; }
+        public List<Employee> Employees { get => employees; }
+        public List<Customer> Customers { get => customers; }
         public string SelectedMethodOfPayment { get => model.MethodOfPayment; set => model.MethodOfPayment = value}
         public Breakfast SelectedBreakfast { get => model.Breakfast; set => model.Breakfast = value; }
         public Event SelectedEvent { get => model.Event; set => model.Event = value; }
