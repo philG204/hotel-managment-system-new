2e43310d1c1a463168c9f672f143e1b137dc8e29
Philipp Gierenz <Philipp.Gierenz@buhmann.schule>
2022-05-13 09:26:57 +0200

    Modified UI, Added new UserControles

--- src/hotel.managment.system.UI/ViewModels/ViewModelMainWindow.cs
+++ src/hotel.managment.system.UI/ViewModels/ViewModelMainWindow.cs
@@ -5,8 +5,10 @@ using hotel_managment_system.Models;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -14,16 +16,49 @@ using System.Windows.Input;
 
 namespace hotel.managment.system.UI
 {
-    public class ViewModelMainWindow
+    public class ViewModelMainWindow : INotifyPropertyChanged
     {
         private object _currentView;
-        
+        private ICommand selectionViewModel;
+        private ICommand referenceDataViewModel;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
         public ViewModelMainWindow()
         {
-            ViewModelSelection Selection = new ViewModelSelection();
-            _currentView = Selection;
+            selectionViewModel = new RelayCommand(ChangeSelection);
+            referenceDataViewModel = new RelayCommand(ChangeReferanceData);
         }
 
-        public object CurrentView { get => _currentView; set => _currentView = value; }
+        private void ChangeReferanceData()
+        {
+            ViewModelReferenceData vmrd = new ViewModelReferenceData();
+            _currentView = vmrd;
+            MessageBox.Show(_currentView.ToString());
+        }
+
+        private void ChangeSelection()
+        {
+            ViewModelSelection vms = new ViewModelSelection();
+            _currentView = vms;
+            MessageBox.Show(_currentView.ToString());
+        }
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+        public ICommand SelectionViewModel { get => selectionViewModel; set => selectionViewModel = value; }
+        public ICommand ReferenceDataButton{ get => referenceDataViewModel; set => referenceDataViewModel = value; }
+
+        protected void OnPropertyChanged([CallerMemberName] string _currentView = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(_currentView));
+        }
     }
 }
---
+++ src/hotel.managment.system.UI/ViewModels/ViewModelReferenceData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hotel.managment.system.UI.UserControls
+{
+    public class ViewModelReferenceData
+    {
+        public ViewModelReferenceData()
+        {
+
+        }
+    }
+}
